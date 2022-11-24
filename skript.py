8c0edaba082fd896f559c322f31db363a4ee226f
alisabu <alisa.bu@bk.ru>
2022-11-24 15:23:24 +0400

    Commit

--- skript.py
+++ skript.py
@@ -1,1 +1,13 @@
-print ("Hello world")
+r = int(input ("Введите количество билетов"))
+itog = 0
+for i in range(r):
+    age = int(input ("Введите возраст"))
+    if age < 18:
+        itog= itog+0
+    elif 18<=age<25:
+        itog=itog+990
+    else:
+        itog= + 1390
+    if r>=3:
+        itog=int(itog*0.9)
+print (itog)
